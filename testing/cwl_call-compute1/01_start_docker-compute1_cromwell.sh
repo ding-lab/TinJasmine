9aea2b4e7a185c0f1a1683688f76eebb164e2c99
Wyczalkowski <m.wyczalkowski@compute1-exec-182.ris.wustl.edu>
2021-09-09 22:36:58 +0000

    Re-organizing demo files

---
+++ testing/cwl_call-compute1/01_start_docker-compute1_cromwell.sh
@@ -0,0 +1,8 @@
+IMAGE="mwyczalkowski/cromwell-runner"
+
+VOLUME_MAP="/storage1/fs1/m.wyczalkowski /home/m.wyczalkowski/Projects "
+
+# absolute path to location of TinJasmine
+CWL_ROOT="/cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/TinJasmine/TinJasmine.dev"
+
+export LSF_DOCKER_NETWORK=host && $CWL_ROOT/submodules/WUDocker/start_docker.sh -I $IMAGE -M compute1 $VOLUME_MAP
--- testing/cwl_call/0_start_docker-MGI_cromwell.sh
+++
@@ -1,3 +0,0 @@
-IMAGE="mwyczalkowski/cromwell-runner"
-
-../../submodules/WUDocker/start_docker.sh -I $IMAGE -M MGI
