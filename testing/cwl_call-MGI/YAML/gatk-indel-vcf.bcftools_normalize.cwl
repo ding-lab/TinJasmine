7d0db54f992240a6a5b7b3fcd6fcd890c5bdd5fe
Wyczalkowski <m.wyczalkowski@compute1-exec-124.ris.wustl.edu>
2021-09-09 23:05:03 +0000

    Cleaning up testing directories
    * Tested on compute1
    * this code not tested on MGI

---
+++ testing/cwl_call-MGI/YAML/gatk-indel-vcf.bcftools_normalize.cwl
@@ -0,0 +1,3 @@
+vcf:  # type "File"
+    class: File
+    path: /cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/TinJasmine/data/C3L-00081-VCF/gatk-indel-vcf/GATK.indel.Final.vcf.gz
--- testing/cwl_call-compute1/01_start_docker-compute1_cromwell.sh
+++ testing/cwl_call-compute1/01_start_docker-compute1_cromwell.sh
@@ -1,6 +1,6 @@
 IMAGE="mwyczalkowski/cromwell-runner"
 
-VOLUME_MAP="/storage1/fs1/m.wyczalkowski /home/m.wyczalkowski/Projects "
+VOLUME_MAP="/storage1/fs1/m.wyczalkowski /home/m.wyczalkowski/Projects /storage1/fs1/m.wyczalkowski/Active/cromwell-data "
 
 # absolute path to location of TinJasmine
 CWL_ROOT="/cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/TinJasmine/TinJasmine.dev"
