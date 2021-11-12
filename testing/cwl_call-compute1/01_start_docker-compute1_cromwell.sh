07b9f8c12f3af8331b16f5b1c21a0c9654f5fc53
Wyczalkowski <m.wyczalkowski@compute1-exec-136.ris.wustl.edu>
2021-11-12 15:26:07 +0000

    Adding demmo workflow for compute1 system

--- testing/cwl_call-compute1/01_start_docker-compute1_cromwell.sh
+++
@@ -1,8 +0,0 @@
-IMAGE="mwyczalkowski/cromwell-runner"
-
-VOLUME_MAP="/storage1/fs1/m.wyczalkowski /home/m.wyczalkowski/Projects /storage1/fs1/m.wyczalkowski/Active/cromwell-data "
-
-# absolute path to location of TinJasmine
-CWL_ROOT="/cache1/fs1/home1/Active/home/m.wyczalkowski/Projects/TinJasmine/TinJasmine.dev"
-
-export LSF_DOCKER_NETWORK=host && $CWL_ROOT/submodules/WUDocker/start_docker.sh -I $IMAGE -M compute1 $VOLUME_MAP
